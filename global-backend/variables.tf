b1e4d0c327729584f0be60cffe8b8d61c31006a3
Doug T. <dtolbertcooke@gmail.com>
2025-11-16 18:20:02 -0500

    updating oidc policy / adding global lambda buckets

--- global-backend/variables.tf
+++ global-backend/variables.tf
@@ -3,12 +3,10 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
-
 variable "state_bucket_name" {
   description = "Name of the S3 bucket for Terraform state"
   type        = string
 }
-
 variable "state_table_name" {
   description = "Name of the DynamoDB table for Terraform locks"
   type        = string
